decb2c71c00ed9dba3b842db638382d1948a72dd
mrandri19 <andrecogno@hotmail.it>
2016-06-29 14:38:28 +0200

    Lexer is working

--- src/main.rs
+++ src/main.rs
@@ -1,66 +1,97 @@
+extern crate regex;
+
+use regex::Regex;
+
 fn main() {
-    println!("Hello, world!");
     let mut lexer = Lexer::new("1+1+(1+1)");
+
+    let mut tokens = Vec::new();
+    while let Some(tok) = lexer.next_token() {
+        tokens.push(tok);
+    }
+    tokens.push(Token::EOF);
+
+    println!("{:?}", tokens);
 }
 
+#[derive(Debug)]
 enum Operators {
     Plus,
     Minus,
     Star,
-    Slash
+    Slash,
 }
 
+#[derive(Debug)]
 enum Token {
     Operator(Operators),
-    Digit(f64),
+    Digit(i32),
     OpenBracket,
-    ClosedBracket
+    ClosedBracket,
+    EOF,
 }
 
-fn operator(input: &str) -> Option<(Token, i32)> {
+fn operator(input: &str) -> Option<(Token, usize)> {
     match input.chars().next().unwrap() {
-        '+' => Some((Token::Operator(Operators::Plus),1)),
-        '-' => Some((Token::Operator(Operators::Minus),1)),
-        '*' => Some((Token::Operator(Operators::Star),1)),
-        '/' => Some((Token::Operator(Operators::Slash),1)),
-        _ => None
+        '+' => Some((Token::Operator(Operators::Plus), 1)),
+        '-' => Some((Token::Operator(Operators::Minus), 1)),
+        '*' => Some((Token::Operator(Operators::Star), 1)),
+        '/' => Some((Token::Operator(Operators::Slash), 1)),
+        _ => None,
     }
 }
 
-fn digit(input: &str) -> Option<(Token, i32)> {
-
+fn digit(input: &str) -> Option<(Token, usize)> {
+    let re = Regex::new("^\\d+").unwrap();
+    if let Some(matches) = re.captures(input) {
+        let digit_str = matches.at(0).unwrap();
+        let digit = digit_str.parse::<i32>().unwrap();
+        return Some((Token::Digit(digit), digit_str.len()));
+    }
+    None
 }
 
-fn openBracket(input: &str) -> Option<(Token, i32)> {
-
+fn open_bracket(input: &str) -> Option<(Token, usize)> {
+    match input.chars().next().unwrap() {
+        '(' => Some((Token::OpenBracket, 1)),
+        _ => None,
+    }
 }
 
-fn closedBracket(input: &str) -> Option<(Token, i32)> {
-
+fn closed_bracket(input: &str) -> Option<(Token, usize)> {
+    match input.chars().next().unwrap() {
+        ')' => Some((Token::ClosedBracket, 1)),
+        _ => None,
+    }
 }
 
-static lexemes: Vec<fn(&str) -> Option<(Token, i32)>> = vec![operator, digit, openBracket, closedBracket];
-
-struct Lexer {
-    input: String,
-    pos: i32
+struct Lexer<'a> {
+    input: &'a str,
+    pos: usize, // lexemes: Vec<fn(&str) -> Option<(Token, usize)>>
 }
 
-impl Lexer {
+impl<'a> Lexer<'a> {
     fn new(input: &str) -> Lexer {
         Lexer {
-            input: input.to_owned(),
-            pos: 0
+            input: input,
+            pos: 0, // lexemes: vec![operator, digit, openBracket, closedBracket]
         }
     }
 
-    fn advance(&mut self, len: i32) {
+    fn advance(&mut self, len: usize) {
         self.pos += len;
+        self.input = &self.input[len..];
     }
 
     fn next_token(&mut self) -> Option<Token> {
-        for lex_func in lexemes {
-            if let Some((tok, len)) = lex_func(&self.input) {
+        let lexemes: Vec<fn(&str) -> Option<(Token, usize)>> =
+            vec![operator, digit, open_bracket, closed_bracket];
+
+        for lexeme in &lexemes {
+            if self.input.is_empty() {
+                return None;
+            }
+            if let Some((tok, len)) = lexeme(&self.input) {
                 self.advance(len);
                 return Some(tok);
             }
